9cc6526e6622178410b3830915e36f62275cc7c2
Rohit1823 <rohitmachale1823@gmail.com>
2021-05-22 21:33:10 +0530

    [Rohit]ADD: Given 3 Floats find the Maximum

--- FindMaximumNumber/FindMaximumNumber/Program.cs
+++ FindMaximumNumber/FindMaximumNumber/Program.cs
@@ -7,6 +7,12 @@ namespace FindMaximumNumber
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Maximum Number using Generics!");
+            Console.WriteLine("Find max Number");
+            MaxNumberCheck MaxInt = new MaxNumberCheck();
+            int output = MaxInt.MaximumIntegerNumber(11, 22, 33);
+            Console.WriteLine(output);
+            float floatoutput = MaxInt.MaximumFloatNumber(111.2f, 22.45f, 78.56f);
+            Console.WriteLine(floatoutput);
         }
     }
 }
