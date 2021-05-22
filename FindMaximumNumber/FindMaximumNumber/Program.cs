82b9fe9255f1609088bb47e7f957f1c3f7e47417
Rohit1823 <rohitmachale1823@gmail.com>
2021-05-22 21:23:47 +0530

    [Rohit]ADD: Given 3 Integers find the Maximum

---
+++ FindMaximumNumber/FindMaximumNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindMaximumNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Maximum Number using Generics!");
+        }
+    }
+}
